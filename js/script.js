69be6bbded4760add7492666a246e874e4278bf6
Akhmad Dany Fathurrasyad <ahkmaddany1@gmail.com>
2024-03-28 01:52:11 +0700

    fourth commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// ini script js
+    function submitForm() {
+        console.log('submit form clicked');
+    }
+
+    document.getElementById('send-button').addEventListener('click', () =>submitForm());
